d1991df1a4bb8df7bcd683ef906a5de10f6c332b
sebahattinozdemir <31370705+sebahattinozdemir@users.noreply.github.com>
2018-11-16 02:26:13 +0300

    Add files via upload

---
+++ 12/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KullaniciKayitDefteri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label9.Text = textBox1.Text;
+            label10.Text = textBox2.Text;
+            label11.Text = comboBox1.Text;
+            label12.Text = textBox3.Text;
+
+            
+        }
+    }
+}
