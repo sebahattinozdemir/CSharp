e85bca8035c4fc47f528f4ff7086eded5e1e4690
sebahattinozdemir <31370705+sebahattinozdemir@users.noreply.github.com>
2018-11-16 02:19:12 +0300

    Add files via upload

---
+++ 3/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_BasicCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x, x1,sum ;
+            x = Convert.ToInt32(textBox1.Text);
+            x1 = Convert.ToInt32(textBox2.Text);
+            sum = x + x1;
+            textBox3.Text = sum.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x, x1, multiply;
+            x = Convert.ToInt32(textBox4.Text);
+            x1 = Convert.ToInt32(textBox5.Text);
+            multiply = x * x1;
+            textBox6.Text = multiply.ToString();
+
+
+        }
+    }
+}
